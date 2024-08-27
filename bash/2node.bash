956c5fcc10d7825ea5f1059b2f4ab2ea02a95119
RtrMmmt <ryotaro.manmoto@gmail.com>
2024-08-27 17:47:45 +0900

    fix

--- bash/2node.bash
+++ bash/2node.bash
@@ -2,6 +2,11 @@
 #PJM -L "rscunit=fx"
 #PJM -L "rscgrp=fx-debug"
 #PJM -L "node=2"
-#PJM --mpi "proc=24"
-#PJM -L "elapse=10:00"
-mpiexec -of 2node -n 8 ./spmv data/Transport.mtx
+#PJM --mpi "proc=64"
+#PJM -L "elapse=30:00"
+mpiexec -n 2 ./spmv data/Transport.mtx
+mpiexec -n 4 ./spmv data/Transport.mtx
+mpiexec -n 8 ./spmv data/Transport.mtx
+mpiexec -n 16 ./spmv data/Transport.mtx
+mpiexec -n 32 ./spmv data/Transport.mtx
+mpiexec -n 64 ./spmv data/Transport.mtx
